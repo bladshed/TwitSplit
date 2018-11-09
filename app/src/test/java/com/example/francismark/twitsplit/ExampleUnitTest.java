1c9391055d8157082c0c21d57ead2abf2dfa005a
francismark <francismarkserrano@yahoo.com>
2018-11-09 19:56:14 +0800

    Modified error message Added test scripts

--- app/src/test/java/com/example/francismark/twitsplit/ExampleUnitTest.java
+++ app/src/test/java/com/example/francismark/twitsplit/ExampleUnitTest.java
@@ -10,8 +10,45 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+
+    /**
+     * This test will check if the input message with more than 50 characters will be divided into 50 characters per line
+     */
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void chunkingMessage() {
+
+        String expectedVal =
+                  "1/2 I can't believe Tweeter now supports chunking\n"
+                + "2/2 my messages, so I don't have to do it myself.";
+
+        String actualVal = MainActivity.splitMessage("I can't believe Tweeter now supports chunking my messages, so I don't have to do it myself.");
+
+        assertEquals(expectedVal, actualVal);
+    }
+
+    /**
+     * This test will check if the input message with less than or equal 50 characters will remain as is
+     */
+    @Test
+    public void notMoreThanFiftyChars() {
+
+        String expectedVal = "Hi! My name is LeBron James.";
+
+        String actualVal = MainActivity.splitMessage("Hi! My name is LeBron James.");
+
+        assertEquals(expectedVal, actualVal);
+    }
+
+    /**
+     * This test will check if the return message will have an error
+     */
+    @Test
+    public void wordWithmoreThanFiftyChars() {
+
+        String expectedVal = "[ERROR] Hello there! ThisShouldReturnAnErrorBecauseThisWordIsMoreThan50Characters end";
+
+        String actualVal = MainActivity.splitMessage("Hello there! ThisShouldReturnAnErrorBecauseThisWordIsMoreThan50Characters end");
+
+        assertEquals(expectedVal, actualVal);
     }
 }
